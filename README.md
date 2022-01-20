338f845f7346136f16d4081d6b2930a6b447a44d
iamnvna <tscobbinah@gmail.com>
2022-01-20 15:33:40 +0000

    Final commit.

---
+++ README.md
@@ -0,0 +1,26 @@
+### Project Seven - 100DaysofCode
+# CS Quiz Game
+This is a simple program (game) which allows users to respond to five questions. 
+
+## Background
+This project is part of my #100DaysOfCode learning streak. I intend to write a simple python program each day. Each project would:
+* be heavily commented for learning purposes;
+* include a README file similar to this one;
+* include an algorithm to solve it without code;
+* include a summary of what I learnt.
+
+## Algorithm
+1. Start.
+2. Create a while loop to allow the program restart after each play.
+3. Create a score variable to hold the number of correct answers.
+4. Using variables and if functions, collect user input and compare them against the correct answer for each question. 
+5. Increase the score variable by one on each correct answer.
+6. Print the score variable count.
+7. Stop.
+
+## What I've learnt
+* I learned to use the while loop to restart a program or run it again from scratch.
+* I have gain better understanding to loops and how they can be applied in projects.
+* I have learnt the application of the .lower() function in development.
+
+For more understanding, watch the full tutorial **[here](https://youtu.be/DLn3jOsNRVE?t=293)** from freeCodeCamp.org
--- main.py
+++ main.py
@@ -47,9 +47,50 @@ while True:
         again = str(input('Want to play again? (Yes/No): '))
         if again.lower() in ('yes', 'no'):
             break
-        print("invalid input.")
+        print("Invalid input.")
     if again.lower() == 'yes':
         continue
     else:
         print("Goodbye!")
         quit()
+
+
+## Commentary
+"""
+Line 1:
+    Line 1 is a while loop that runs the program as long as the out put is true. For the output
+    to be True however, the block of while loop between line 46 and 55 must equate to two. This 
+    is one way of restarting a program after it has finished executing. Learnt it on StackOverflow.
+
+Line 2:
+    This is a basic print command that welcomes users to the game.
+    
+Line 3: 
+    This line of code accepts an input (yes/no) from the user. The input determines whether the
+    user wants to begin/continue play or quit the program.
+
+Line 5-6:
+    This block of code evaluates the response from the user from line 3 and performs the required
+    action. The .lower() function appended to the variable converts the input type to lowercase
+    to effectively match the code.
+
+Line 8:
+    A score global variable is created to hold the total score (correct answers) from the game.
+
+Line 9-14 et al.:
+    The variable answer is assigned the input from the question asked. If the input evaluates to
+    the right answer in Line 10, the program prints 'Correct!' as indicated on line 11, and proceeds
+    to add one to the score (Line 12). If the answer (user input) to the question evaluates to false,
+    the program prints 'Incorrect!' as indicated on line 14. 
+
+Line 44:
+    Through the print statement and basic concatenation, the total score and percentage from the 
+    game is printed out to the user. Note that all integers and float variable types are converted
+    tp strings before concatenated in the print statement.
+    
+Line 46-55:
+    This block of code collects user input (yes/no) to tell whether to close the game or run it
+    once more. In line 48, the code evaluates the user input to either yes or no, and prints out 
+    'Invalid input.' (Line 50) to anything apart from the two. The program proceeds to run if the
+    user answers 'yes' but quits and prints out 'Goodbye!' if the user answers no. 
+"""
